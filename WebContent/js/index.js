35c4c7dd3bdbbde9202ec1547a20f288cbf63c26
Gavish <gavishgulati@yahoo.com>
2015-11-06 21:22:24 -0500

    welcome Page, author gg

---
+++ WebContent/js/index.js
@@ -0,0 +1,10 @@
+function manClick(){
+	$("#bodyList").hide();
+	$("#manBodyParts").removeClass("none");
+	$("#manBodyParts").addClass("animated bounceInLeft");
+}
+function womanClick(){
+	$("#bodyList").hide();
+	$("#womanBodyParts").removeClass("none");
+	$("#womanBodyParts").addClass("animated bounceInLeft");
+}
